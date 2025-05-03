e43ef22f2c1cd3731e87b54895aa5c63500ab6ec
FireIce421 <92722498+FireIce421@users.noreply.github.com>
2025-05-03 20:42:27 +0200

    dry out better effect
    the source uses an older ver ok

--- primary.lua
+++ primary.lua
@@ -457,16 +457,18 @@ SMODS.Joker {
   loc_txt = {
     name = 'Dry Out',
     text = {
-      "{C:mult}+#1# Mult{} for every Non-Face Card.",
-      "{C:inactive}Yeah, it has to score.."
-    }
+      'For each round without selling anything, gain {C:mult}+#1#{} Mult, otherwise {C:attention}reset{}',
+      '{C:inactive}Activates an additional time per scored card',
+      '{C:inactive}(Currently: {C:mult}+#2#{}{C:inactive}){}'
+      }
   },
   loc_vars = function(self, info_queue, card)
-    return { vars = { card.ability.extra.mult } }
+      return { vars = { card.ability.extra.mult_mod, card.ability.extra.mult } }
   end,
-  config = { extra = { mult = 6 } },
+  config = { extra = { mult = 0, mult_mod = 2 } },
   rarity = 'gj_norm',
   unlocked = true,
+  discovered = true,
   mainlevel = true,
   firstrelease = true,
   blueprint_compat = true,
@@ -474,15 +476,25 @@ SMODS.Joker {
   pos = { x = 3, y = 0 },
   cost = 8,
   calculate = function(self, card, context)
-    if context.individual and context.cardarea == G.play then
-      if not context.other_card:is_face() then
-      return {
-         mult_mod = card.ability.extra.mult,
-         message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
-      }
-    end
-  end
-end
+      if context.setting_blind and not context.blueprint and not context.retrigger_joker then
+          card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_mod
+      end
+      if context.cardarea == G.play and context.main_scoring then
+          return {
+          mult = card.ability.extra.mult,
+          message = localize { type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult } }
+          }
+      end
+      if context.joker_main then
+          return {
+          mult = card.ability.extra.mult,
+          message = localize { type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult } }
+          }
+      end
+      if context.selling_card and context.cardarea == G.jokers then
+          card.ability.extra.mult = 0
+      end
+  end,
 }
  
  SMODS.Joker {
